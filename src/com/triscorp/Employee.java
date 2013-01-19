8a5a9e9a7d22324fe3b8f7eed8d68171f5d347f2
tristian o'brieb <tristian2@hotmail.com>
2013-01-19 23:49:38 +0000

    for the final Assignment of ISE
    a sample project to test out Mac development, IceSCRUM and a simple
    "project"

---
+++ src/com/triscorp/Employee.java
@@ -0,0 +1,11 @@
+package com.triscorp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tristian2
+ * Date: 19/01/2013
+ * Time: 17:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+}
---
+++ src/com/triscorp/Entry.java
@@ -0,0 +1,18 @@
+package com.triscorp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tristian2
+ * Date: 19/01/2013
+ * Time: 17:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Entry {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Application starting!"); // Display the string.
+    }
+
+
+}
+
