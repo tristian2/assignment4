1cb1a7000ffebc9f65696e724ec7681a8a3cb005
tristian o'brieb <tristian2@hotmail.com>
2013-02-03 19:13:51 +0000

    implement a basic sine curve

--- src/com/triscorp/Entry.java
+++ src/com/triscorp/Entry.java
@@ -9,8 +9,34 @@ package com.triscorp;
  */
 public class Entry {
 
+
+    private static int consoleWidth = 40;
+
     public static void main(String[] args) {
         System.out.println("Hello Application starting!"); // Display the string.
+
+
+
+        for (double  i=0;i<2*Math.PI; i=i+0.1)
+        {
+            System.out.println(aLine(Math.sin(i)));
+        }
+    }
+
+
+    public static void sineCurve() {
+
+    }
+
+
+    public static String aLine(double ratio)
+    {
+
+        StringBuilder line = new StringBuilder();
+        for (int i=0;i<consoleWidth*ratio;i++) {
+            line.append("*");
+        }
+        return line.toString();
     }
 
 
