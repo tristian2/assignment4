43b92106586b2e99834c101ebe8fb03bb336aeea
tristian o'brieb <tristian2@hotmail.com>
2013-02-10 16:01:03 +0000

    fix bug

---
+++ src/com/triscorp/ScientificCalculator/Preferences.java
@@ -0,0 +1,12 @@
+package com.triscorp.ScientificCalculator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tristian2
+ * Date: 10/02/2013
+ * Time: 15:58
+ * To change this template use File | Settings | File Templates.
+ * TODO for the "fixbug" task add preferences
+ */
+public class Preferences {
+}
