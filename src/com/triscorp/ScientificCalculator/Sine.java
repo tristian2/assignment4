72b02ef9b0b8ba9e26586f762c40e14ffdb74181
tristian o'brieb <tristian2@hotmail.com>
2013-02-10 14:00:46 +0000

    T7 Show a sine curve

--- src/com/triscorp/ScientificCalculator/Sine.java
+++ src/com/triscorp/ScientificCalculator/Sine.java
@@ -1,30 +1,33 @@
 package com.triscorp.ScientificCalculator;
 
+import sun.tools.tree.SuperExpression;
+
 /**
  * Created with IntelliJ IDEA.
  * User: tristian2
  * Date: 03/02/2013
  * Time: 20:57
  * To change this template use File | Settings | File Templates.
+ * produces a sine wave
  */
-public class Sine extends BaseGraph  {
+public class Sine extends BaseGraph {
 
+    public Sine() {
+        setGraphLabel("A sine wave");
+    }
 
-    public String graph()
-    {
+    public String graph() {
 
         StringBuilder sb = new StringBuilder();
 
-        for (double  i=0;i<2*Math.PI; i=i+0.1)
-        {
+        sb.append(getGraphLabel()+"\n");
+        for (double i = 0; i < 2 * Math.PI; i = i + 0.1) {
             sb.append(aLine(Math.sin(i)));
         }
 
         return sb.toString();
 
     }
-
-
 
 
 }
